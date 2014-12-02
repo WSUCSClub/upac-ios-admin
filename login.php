c45acefa8b8a787cc054a2d1cdfad592bcbcfa91
Richard Marquez <rmarquez14@winona.edu>
2014-12-01 23:21:45 -0600

    removed ugly ios button styles

--- login.php
+++ login.php
@@ -69,7 +69,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       </div>
 
       </main>
-      <footer>
+      <footer class="login">
           <p>Need help? Shoot me an email at <a href="mailto:rmarquez14@winona.edu?subject=WSU%20UPAC%20App%20administration%20help">rmarquez14@winona.edu</a></p>
       </footer>
     </div> <!-- END CONTAINER -->
